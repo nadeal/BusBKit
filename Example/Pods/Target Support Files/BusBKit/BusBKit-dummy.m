ecde1d4ef9fb074a39d9bd5a61642374d8832fc7
nadeal <1973943303@qq.com>
2021-10-25 10:49:23 +0800

    0.1.1
    0.1.1

---
+++ Example/Pods/Target Support Files/BusBKit/BusBKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BusBKit : NSObject
+@end
+@implementation PodsDummy_BusBKit
+@end
--- Example/Pods/Target Support Files/Pods-BusBKit_Example/Pods-BusBKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-BusBKit_Example/Pods-BusBKit_Example-frameworks.sh
@@ -175,9 +175,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BusBKit/BusBKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YakerKit/YakerKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BusBKit/BusBKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YakerKit/YakerKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
